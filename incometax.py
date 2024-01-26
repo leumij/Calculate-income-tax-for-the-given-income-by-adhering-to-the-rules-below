0f76eaef9869f50f3b7c3a73fe30d97a71bebe1e
leumij <cjimuel74@gmail.com>
2024-01-26 19:23:22 +0800

    pseudocode

---
+++ incometax.py
@@ -0,0 +1,7 @@
+# For example, suppose the taxable income is 45000, and the income tax payable is
+# 10000*0% + 10000*10%  + 25000*20% = $6000.
+
+# Pseudocode
+# Ask user for Total income
+# Solve for income tax
+# Print for total tax
